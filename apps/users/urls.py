fe8a878e31ef1a35fa4ad0af4c98d9936cf4ffa6
hengage <tunecarterkillz@gmail.com>
2022-08-05 02:50:22 +0100

    Refactor: moved user auth url path to users.urls

---
+++ apps/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+
+urlpatterns = [ 
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+]
--- djoser_user_auth/urls.py
+++ djoser_user_auth/urls.py
@@ -1,14 +1,8 @@
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/auth/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.jwt')),
-
+    path('api/', include('users.urls')),
 ]
