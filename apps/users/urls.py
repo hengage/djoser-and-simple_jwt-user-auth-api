03763630fb93de46505c3e614d80fd6d4acbd858
hengage <tunecarterkillz@gmail.com>
2022-08-06 11:38:22 +0100

    Refactor: similar token obtain endpoint with jwt
    refactored token endpoints

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from djoser import views as DjoserViews
 
 
 urlpatterns = [ 
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/users/logout', DjoserViews.TokenDestroyView.as_view(), name='logout'), 
