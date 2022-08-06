25e812845d806a7e9eb513bef44f90f5c7197aa2
hengage <tunecarterkillz@gmail.com>
2022-08-06 11:36:03 +0100

    Feature: token desytroy

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from djoser import views as DjoserViews
 
 
 urlpatterns = [ 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('auth/users/logout', DjoserViews.TokenDestroyView.as_view(), name='logout'), 
 ]
