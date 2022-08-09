db3b29bee704eae4bdd826c314fc7a2051364ba8
hengage <tunecarterkillz@gmail.com>
2022-08-09 02:31:26 +0100

    Refactor: removed sjwt token code

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from djoser import views as DjoserViews
 
 
 urlpatterns = [ 
-    # path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('auth/users/logout', DjoserViews.TokenDestroyView.as_view(), name='logout'), 
