847a49fad51e546cb708ed508b3cbd64f781e121
hengage <tunecarterkillz@gmail.com>
2022-08-12 03:04:41 +0100

    Feature: changed database settings

--- djoser_user_auth/settings.py
+++ djoser_user_auth/settings.py
@@ -81,13 +81,16 @@ WSGI_APPLICATION = 'djoser_user_auth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -206,6 +209,3 @@ SWAGGER_SETTINGS = {
 
 django_heroku.settings(locals())
 
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
