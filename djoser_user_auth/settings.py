141036c1ba591d7fcee1e4cad212c9808cb43c7d
hengage <tunecarterkillz@gmail.com>
2022-08-12 03:34:02 +0100

    Feature: set up postgresql for database

--- djoser_user_auth/settings.py
+++ djoser_user_auth/settings.py
@@ -16,10 +16,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r81h8mty!p$0g2^4ephgz1(alkmnfj9*y5o91=t6#-*v4dfk$y'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
