4526924c92d36c2267f955d57c61b431e48853a6
hengage <tunecarterkillz@gmail.com>
2022-09-22 23:07:00 +0100

    Feature: email transaction smtp setup

--- djoser_user_auth/settings.py
+++ djoser_user_auth/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     'djoser',
     'rest_framework_simplejwt',
-    'drf_yasg'
+    'drf_yasg',
+    # 'anymail',
 ]
 
 MIDDLEWARE = [
@@ -135,7 +136,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp-relay.sendinblue.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = '2525'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
@@ -187,7 +193,7 @@ DJOSER = {
         # 'user_delete': ['rest_framework.permissions.CurrentUserOrAdmin'],
         # 'user': ['rest_framework.permissions.CurrentUserOrAdmin'],
         # 'user': ['rest_framework.permissions.AllowAny'],
-        'user_list': ['rest_framework.permissions.IsAdminUser'],
+        'user_list': ['rest_framework.permissions.AllowAny'],
         'token_create': ['rest_framework.permissions.AllowAny'],
         'token_destroy': ['rest_framework.permissions.IsAuthenticated'],
     }
